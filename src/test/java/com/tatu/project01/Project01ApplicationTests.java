487a85b1ed35ec26b64e2e1c66f2e1ade10c1494
unknown <hakimovyahyo9@gmail.com>
2021-12-26 10:01:06 +0500

    firs commit

---
+++ src/test/java/com/tatu/project01/Project01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatu.project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
